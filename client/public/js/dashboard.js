9f1ba1b9b5518910cbf0f0a895703c979aeaf20f
Bowen <wangbowen0204@gmail.com>
2018-10-21 14:28:17 -0400

    Converted tasks to expanadable blocks

--- client/public/js/dashboard.js
+++ client/public/js/dashboard.js
@@ -3,3 +3,19 @@ function changeTimeDisplayed(timeToDisplay, state){
 	console.log(timeToDisplay, state)
 
 }
+
+// Convert the Array of Objects into Dashboard Tasks
+function readOutputs(assignments){
+	assignments.forEach(function(element){
+		/* Example for creating new time block
+		var para = document.createElement("p");
+		var node = document.createTextNode("This is new.");
+		para.appendChild(node);
+		var element = document.getElementById("div1");
+		element.appendChild(para);
+		*/ 
+	});
+}
+
+
+
